4307740c2bb87af2aa24bcede3cc057d03706a46
MoAnsarMansoorie <moansarmansoorie@gmail.com>
2024-10-27 16:48:17 +0530

    test api route created

---
+++ api/controllers/test.controller.js
@@ -0,0 +1,3 @@
+export const testController = (req, res) => {
+    return res.status(200).send("hello test server || api routes is working")
+}
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectDb from "./db/connectDb.js"
+import userRouter from "./routes/user.route.js"
 
 dotenv.config()
 
@@ -8,9 +9,7 @@ const app = express()
 
 connectDb()
 
-app.get("/test", (req, res) => {
-    return res.status(200).send("hello test server")
-})
+app.use("/api/v1/user", userRouter)
 
 app.listen(8080, (req, res) => {
     console.log(`Server is started at http://localhost:8080`)
